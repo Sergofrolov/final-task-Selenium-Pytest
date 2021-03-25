5875f1f8a23a0faef867abf68618db8aaff46108
Sergofrolov <zergen.z@mail.ru>
2021-03-25 18:09:33 +0300

    добавлены проверки для login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,15 @@
 from pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()
+    page = MainPage(browser, link)
     # открываем страницу
-    page.go_to_login_page()
+    page.open()
     # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
+    page.go_to_login_page()
     # выполняется проверка наличия кнопки авторизации
+    page.should_be_login_link()
+    # выполняются проверки страницы авторизации
+    login_page.should_be_login_page()
